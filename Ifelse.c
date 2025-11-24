02517aaf783b826a73cc74d08ac3508fc2bebc91
mokshiprasad <25kq1a5430@pace.ac.in>
2025-11-24 22:23:24 +0530

    Update and rename README.md to Ifelse.c

---
+++ Ifelse.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+int char;
+clrscr();
+printf("Enter the char");
+scanf("%c",ch);
+if(ch='a' !!ch='e' !!ch='i' !!ch='0' !!ch='u');
+{
+printf("given char is vowel");
+}
+else
+{
+printf("given char is consonant");
+}
+getch();
+}
