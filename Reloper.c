f292d18d3b6e1226217ff6ba4b7ae49a51e3ac07
mokshiprasad <25kq1a5430@pace.ac.in>
2025-11-24 21:25:39 +0530

    Add Reloper.c with basic comparison operations

---
+++ Reloper.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b;
+clrscr();
+a=10;
+b=20;
+printf ("a=%d\t b=%d", a,b);
+printf ("a/n(a>b)=%d",a>b);
+printf ("a/n(a<b)=%d",a<b);
+getch();
+}
