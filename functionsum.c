38eec24ddfa3980445d66c951bd0aeadf1a80226
mokshiprasad <25kq1a5430@pace.ac.in>
2025-11-26 20:47:06 +0530

    Add files via upload

---
+++ functionsum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int n,s=0;
+ int sum();
+ printf("enter the numbers");
+ printf("%d",&n);
+ printf("sum of the n numbers");
+ getch();
+}
+int sum()
+{
+ int i, n,s=0;
+ for( i=1;n<=1;i++)
+ s=s+1;
+ return s;
+ }
